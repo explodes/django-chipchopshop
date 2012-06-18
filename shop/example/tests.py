3f202f27174ba3f0e33bec83d0c63d9b3a066fc9
Evan Leis <evan.leis@wearetopsecret.com>
2012-06-18 13:28:56 -0600

    Temporarily abandoning project.

--- shop/example/tests.py
+++ shop/example/tests.py
@@ -18,3 +18,31 @@ class TestInheritance(TestCase):
 
         assert models.Product.objects.all()[0].book == book
         assert models.Product.objects.all()[0].get_subtype_instance() == book
+
+    def test_cart_with_products(self):
+        from shop.example import models
+
+        book = models.Book()
+        book.isbn_13 = '0987123456789'
+        book.price = '1.00'
+        book.save()
+
+        bookvariant = models.BookVariant()
+        bookvariant.item = book
+        bookvariant.save()
+
+        cart = models.Cart()
+        cart.save()
+
+        cartitem1 = models.CartItem()
+        cartitem1.variant = bookvariant
+        cartitem1.quantity = 2
+        cartitem1.save()
+
+        cartitem2 = models.CartItem()
+        cartitem2.variant = bookvariant
+        cartitem2.quantity = 3
+        cartitem2.save()
+
+        print cart.price()
+
